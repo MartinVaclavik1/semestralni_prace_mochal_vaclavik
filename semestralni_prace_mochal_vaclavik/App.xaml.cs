7994d3001aa0789b14cc7d04862789c5f8a1b27e
Martin Václavík <146117943+MartinVaclavik1@users.noreply.github.com>
2025-10-24 11:00:50 +0200

    Přidejte soubory projektu.

---
+++ semestralni_prace_mochal_vaclavik/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace semestralni_prace_mochal_vaclavik
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
