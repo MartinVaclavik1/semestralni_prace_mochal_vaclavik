1f407bde6882185b65fac2b62eea3ec7c875a1dd
Oliver Mochal <oliver.mochal@student.upce.cz>
2025-11-21 14:17:13 +0100

    idk přidal jsem tam nějaký voalni do databaze alae nejde to

--- semestralni_prace_mochal_vaclavik/MainWindow.xaml.cs
+++ semestralni_prace_mochal_vaclavik/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,67 +10,131 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client; // Důležité: Přidat toto using
 
 namespace semestralni_prace_mochal_vaclavik
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // 1. Zde nastavte připojovací řetězec (zkopírujte údaje z přihlašování)
+        // Všimněte si formátu s dvojtečkou pro SID: fei-sql3.upceucebny.cz:1521:BDAS
+
+        string connectionString = "User Id=ST72536;Password=||DejTamSvojeHeslo||;Data Source=fei-sql3.upceucebny.cz:1521:BDAS;";
+
         public MainWindow()
         {
             InitializeComponent();
-            
-            Prestupky.IsEnabled = false;    //dokud bude uživatel odhlášen, tak některé okna budou disabeled jako toto
-            Prestupky.Visibility = Visibility.Hidden;
-            Okna.UseLayoutRounding = true;
-            //TODO přidat popup okno s přihlášením - možnost vytvořit nový účet a pokračovat jako host bez přihlášení
+
+            // Načíst data hned po spuštění (nebo to můžete dát až po kliknutí na záložku)
+            //NacistKontakty();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Metoda pro načtení dat do tabulky Kontakty
+        private void NacistKontakty()
         {
-            //možná budeme využívat vytváření nových oken za běhu
-            var testItem = new TabItem();
-            testItem.Header = "Test nového okna";
-            Okna.Items.Add(testItem);
+            try
+            {
+                // Vytvoření připojení
+                using (OracleConnection pripojeni = new OracleConnection(connectionString))
+                {
+                    pripojeni.Open();
 
-            Prestupky.Visibility = Visibility.Hidden; //nebo možnost schovávat a zobrazovat okna
+                    // SQL dotaz - spojíme Policistu s Hodností, aby to hezky vypadalo
+                    string sql = @"
+                        SELECT 
+                            p.jmeno, 
+                            p.prijmeni, 
+                            h.nazev AS hodnost, 
+                            s.nazev AS stanice 
+                        FROM policista p
+                        JOIN hodnost h ON p.hodnost_idhodnosti = h.idhodnosti
+                        JOIN policejni_stanice s ON p.policejni_stanice_idstanice = s.idstanice";
+
+                    using (OracleCommand cmd = new OracleCommand(sql, pripojeni))
+                    {
+                        // Použijeme DataAdapter pro naplnění tabulky
+                        OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        // Napojení dat do vašeho DataGridu v XAML
+                        KontaktyGrid.ItemsSource = dt.DefaultView;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Chyba při načítání kontaktů: " + ex.Message);
+            }
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        // Příklad načtení detailu uživatele (pro záložku Můj účet)
+        // Tuto metodu zavolejte, když budete chtít načíst konkrétního člověka (např. ID 1)
+        private void NacistDetailUzivatele(int idUzivatele)
         {
+            try
+            {
+                using (OracleConnection pripojeni = new OracleConnection(connectionString))
+                {
+                    pripojeni.Open();
 
+                    // Příklad dotazu pro občana
+                    string sql = "SELECT jmeno, prijmeni, ulice, postovnismerovacicislo, obec, zeme " +
+                                 "FROM obcan o JOIN adresa a ON o.adresa_idadresy = a.idadresy " +
+                                 "WHERE o.uzivatel_iduzivatele = :id";
+
+                    using (OracleCommand cmd = new OracleCommand(sql, pripojeni))
+                    {
+                        cmd.Parameters.Add(new OracleParameter("id", idUzivatele));
+
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Naplnění vašich TextBoxů z XAML
+                                JmenoTxt.Text = reader["jmeno"].ToString();
+                                PrijmeniTxt.Text = reader["prijmeni"].ToString();
+                                UliceTxt.Text = reader["ulice"].ToString();
+                                PSCTxt.Text = reader["postovnismerovacicislo"].ToString();
+                                ZemeTxt.Text = reader["zeme"].ToString();
+                                // AdresaTxt asi chcete složit z ulice a čísla, pokud to v DB máte
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Chyba detailu: " + ex.Message);
+            }
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
+        // Obsluha přepínání záložek (máte ji v XAML jako Okna_SelectionChanged)
         private void Okna_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void PotvrditBtn_Click(object sender, RoutedEventArgs e)
-        {
-
+            if (e.Source is System.Windows.Controls.TabControl)
+            {
+                if (Kontakty.IsSelected)
+                {
+                    NacistKontakty();
+                }
+                else if (Ucet.IsSelected)
+                {
+                    // Tady byste normálně předal ID přihlášeného uživatele
+                    // Pro test dáme natvrdo ID 1, pokud v DB existuje
+                    NacistDetailUzivatele(1);
+                }
+            }
         }
 
         private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
         {
+            // Zde bude filtrování kontaktů
+        }
 
+        private void PotvrditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Uložení změn
         }
     }
 }
